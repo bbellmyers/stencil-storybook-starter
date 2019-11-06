cf9a5cf035f46aebc7c17fa9d96f3f4158a1671b
BellMyers, Bruce M <Bruce.M.Bellmyers@HealthPartners.Com>
2019-11-06 14:25:32 -0600

    Stencil + Storybook
    * Stencil component project from cli
    * Add storybook/react per cli
    * wire together, sharing www directory
    * To test, run `npm start`.  Component storybook page works in Chrome, but not IE11 on port 6006.
    * Note that Stencil start page on port 3333 works in both.

---
+++ stories/stencil-component.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default {
+  title: 'Components',
+};
+
+export const component = () => <my-component first="Stencil" last="'Don't call me a framework' JS"></my-component>;
